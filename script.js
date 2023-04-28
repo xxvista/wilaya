92ddb5b387df705027e505e76fde6f39826276ac
xxvista <60241139+xxvista@users.noreply.github.com>
2023-04-28 15:50:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-// Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
-// BUG TODO FIXME
-/*
-console.log(document.querySelector(".center__message").textContent);
-document.querySelector(".center__message").textContent = "Correct Number!";
-
-document.querySelector(".number").textContent = 13;
-
-document.querySelector(".left__guess").value = 0;*/
 const wilaya = [
   0,
   "Adrar",
@@ -76,8 +67,8 @@ document.querySelector(".right__highscore").innerHTML =
   localStorage.getItem("highscore");
 
 let wilayaNumber = Math.trunc(Math.random() * 58) + 1;
-document.querySelector(".wilaya").textContent = wilaya[wilayaNumber];
-document.querySelector(".left__guess").value = 0;
+displayWilaya() wilaya[wilayaNumber];
+guessValue() 0;
 
 //let oldNumbers = [];
 let i = -1;
@@ -86,16 +77,43 @@ let score = 0;
 
 console.log(wilayaNumber);
 
+const displayMessage = function (message) {
+  document.querySelector(".center__message").textContent = message;
+};
+
+const changeScore = function (score) {
+  document.querySelector(".right__score").textContent = score;
+};
+
+const displayWilaya = function (wilaya){
+  document.querySelector(".wilaya").textContent = wilaya;
+};
+
+const backgroundChange = function (color) {
+  document.querySelector("body").style.backgroundImage = color;
+};
+
+const buttonText = function (text) {
+  document.querySelector(".left__check").textContent = text;
+};
+
+const guessValue = function (value) {
+  document.querySelector(".left__guess").value = value;
+};
+
+const heartsEmoji = function (emoji) {
+  document.querySelector(".right__hearts").textContent = emoji;
+};
+
 document.querySelector(".head__again").addEventListener("click", function () {
-  document.querySelector(".right__score").textContent = 0;
-  document.querySelector(".center__message").textContent = "Start guessing...";
+  changeScore(0);
+  displayMessage("Start Guessing...");
   wilayaNumber = Math.trunc(Math.random() * 58) + 1;
-  document.querySelector(".wilaya").textContent = wilaya[wilayaNumber];
-  document.querySelector("body").style.backgroundImage =
-    "radial-gradient(#343a40, #212529)";
-  document.querySelector(".left__check").textContent = "Check!";
-  document.querySelector(".left__guess").value = 0;
-  document.querySelector(".right__hearts").textContent = "❤️❤️❤️";
+  displayWilaya(wilaya[wilayaNumber]);
+  backgroundChange("radial-gradient(#343a40, #212529)");
+  buttonText("Check!");
+  guessValue(0);
+  heartsEmoji("❤️❤️❤️");
   document.querySelector(".line-1").style.backgroundColor = "#fff";
   document.querySelector(".line-2").style.backgroundColor = "#fff";
 });
@@ -105,35 +123,30 @@ document.querySelector(".left__check").addEventListener("click", function () {
   const button = document.querySelector(".left__check").textContent;
   const highScore = document.querySelector(".right__highscore").textContent;
 
-  document.querySelector(".wilaya").textContent = wilaya[wilayaNumber];
+  displayWilaya(wilaya[wilayaNumber]);
 
   if (button === "Again!") {
-    document.querySelector(".right__score").textContent = 0;
-    document.querySelector(".center__message").textContent =
-      "Start guessing...";
+    changeScore(0);
+    displayMessage("Start guessing...");
     wilayaNumber = Math.trunc(Math.random() * 58) + 1;
-    document.querySelector(".wilaya").textContent = wilaya[wilayaNumber];
-    document.querySelector("body").style.backgroundImage =
-      "radial-gradient(#343a40, #212529)";
-    document.querySelector(".left__check").textContent = "Check!";
-    document.querySelector(".left__guess").value = 0;
-    document.querySelector(".right__hearts").textContent = "❤️❤️❤️";
+    displayWilaya(wilaya[wilayaNumber]);
+    backgroundChange("radial-gradient(#343a40, #212529)");
+    buttonText("Check!");
+    guessValue(0);
+    heartsEmoji("❤️❤️❤️");
     document.querySelector(".line-1").style.backgroundColor = "#fff";
     document.querySelector(".line-2").style.backgroundColor = "#fff";
   } else if (button === "Next..") {
-    document.querySelector(".wilaya").textContent = wilaya[wilayaNumber];
+    displayWilaya(wilaya[wilayaNumber]);
     console.log(wilayaNumber);
     document.querySelector("body").style.transition =
       "transition: background-image 2s ease";
-    document.querySelector("body").style.backgroundImage =
-      "radial-gradient(#343a40, #212529)";
-    document.querySelector(".left__guess").value = "";
-    document.querySelector(".center__message").textContent =
-      "Start guessing...";
-    document.querySelector(".left__check").textContent = "Check!";
+    backgroundChange("radial-gradient(#343a40, #212529)");
+    guessValue("");
+    displayMessage("Start guessing...");
+    buttonText("Check!");
   } else if (!guess || guess < 0 || guess > 58) {
-    document.querySelector(".center__message").textContent =
-      "⛔ Not w valid Number!";
+    displayMessage("⛔ Not w valid Number!");
     // Win or next
   } else if (guess === wilayaNumber) {
     document.querySelector(".line-1").style.backgroundColor = "#fff";
@@ -144,25 +157,22 @@ document.querySelector(".left__check").addEventListener("click", function () {
     ).textContent = `🎊 Correct ${wilayaNumber} is for ${wilaya[wilayaNumber]}`;
     //oldNumbers.push(wilayaNumber);
     score++;
-    document.querySelector(".right__score").textContent = score;
+    changeScore(score) ;
     wilaya[wilayaNumber] = 0;
-    //  document.querySelector(".left__guess").value = 0;
+    //  guessValue() 0;
     // Store
 
     do {
       wilayaNumber = Math.trunc(Math.random() * 58) + 1;
     } while (wilaya[wilayaNumber] === 0 && score < 58);
     if (score === 58) {
-      document.querySelector(".center__message").textContent =
-        "Geiuss! You Win 🎊";
+      displayMessage("Geiuss! You Win 🎊");
       document.querySelector(".right__highscore").textContent = score;
-      document.querySelector("body").style.backgroundImage =
-        "radial-gradient(#51cf66, #40c057)";
+      backgroundChange("radial-gradient(#51cf66, #40c057)");
     } else {
-      document.querySelector("body").style.backgroundImage =
-        "radial-gradient(#51cf66, #40c057)";
+      backgroundChange("radial-gradient(#51cf66, #40c057)");
 
-      document.querySelector(".left__check").textContent = "Next..";
+      buttonText("Next..");
       if (score > highScore) {
         document.querySelector(".right__highscore").textContent = score;
         localStorage.setItem("highscore", score);
@@ -172,28 +182,25 @@ document.querySelector(".left__check").addEventListener("click", function () {
     /// Too High or low
   } else if (guess !== wilayaNumber) {
     if (heart === 0) {
-      document.querySelector(".right__hearts").textContent = "💔💔💔";
-      document.querySelector(
-        ".center__message"
-      ).textContent = `💥 You lost the game it's ${wilayaNumber}`;
-      document.querySelector(".left__check").textContent = "Again!";
-      document.querySelector("body").style.backgroundImage =
-        "radial-gradient(#ff8787, #ff6b6b)";
+      heartsEmoji("💔💔💔");
+      displayMessage(`💥 You lost the game it's ${wilayaNumber}`);
+      buttonText("Again!");
+      backgroundChange("radial-gradient(#ff8787, #ff6b6b)");
       document.querySelector(".line-1").style.backgroundColor = "#fff";
       document.querySelector(".line-2").style.backgroundColor = "#fff";
       i = -1;
       heart = 2;
       score = 0;
     } else {
-      document.querySelector(".center__message").textContent =
+      displayMessage(
         guess > wilayaNumber
           ? "💔Too high! 📈 Keep guessing"
-          : "💔Too low! 📉 Keep guessing";
-      document.querySelector(".right__hearts").textContent =
-        heart === 2 ? "❤️❤️💔" : "❤️💔💔";
+          : "💔Too low! 📉 Keep guessing"
+      );
+      heartsEmoji(heart === 2 ? "❤️❤️💔" : "❤️💔💔");
       //score--;TODO
       heart--;
-      document.querySelector(".right__score").textContent = score;
+      changeScore(score);
       document.querySelector(".line-1").style.backgroundColor = "#ff6b6b";
       document.querySelector(".line-2").style.backgroundColor = "#ff6b6b";
     }
